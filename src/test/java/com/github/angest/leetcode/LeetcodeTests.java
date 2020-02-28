5771a84f8d97d58340da5c8b45d2b606c98322eb
Meng Zhou <meng.zhou@tutis.com.au>
2020-02-28 21:44:33 +1000

    new project

---
+++ src/test/java/com/github/angest/leetcode/LeetcodeTests.java
@@ -0,0 +1,53 @@
+package com.github.angest.leetcode;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class LeetcodeTests {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    protected <T> T getValue(String src, Class<T> valueType) {
+        try {
+            return objectMapper.readValue(getJsonString(src), valueType);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected <T> T getValue(String src, TypeReference<T> valueTypeRef) {
+        try {
+            return objectMapper.readValue(getJsonString(src), valueTypeRef);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getJsonString(String src) throws IOException, URISyntaxException {
+        String file = src + ".json";
+        String key = null;
+
+        int index = src.indexOf('.');
+        if (index >= 0) {
+            file = src.substring(0, index) + ".json";
+            key = src.substring(index + 1);
+        }
+
+        URL url = this.getClass().getResource(file);
+        if (key != null) {
+            Map<String, ?> map = objectMapper.readValue(url, new TypeReference<Map<String, ?>>() {
+            });
+            return objectMapper.writeValueAsString(map.get(key));
+        } else {
+            Path path = Paths.get(url.toURI());
+            return new String(Files.readAllBytes(path));
+        }
+    }
+}
