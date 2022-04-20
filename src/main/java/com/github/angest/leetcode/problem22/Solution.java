7f0af06c1fcc1f0ce4a76ad45dc194e5071e40c5
Meng Zhou <meng.zhou@tutis.com.au>
2022-04-20 21:27:07 +1000

    problem22 - faster than 62.33%

--- src/main/java/com/github/angest/leetcode/problem22/Solution.java
+++ src/main/java/com/github/angest/leetcode/problem22/Solution.java
@@ -1,32 +1,41 @@
 package com.github.angest.leetcode.problem22;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        result.add("");
+
+        List<char[]> charsList = new ArrayList<>();
+        charsList.add(new char[]{0, 0});
 
         for (int i = 0; i < 2 * n; i++) {
-            String[] strs = result.toArray(result.toArray(new String[0]));
-            result.clear();
-            for (String str : strs) {
-                int left = 0;
-                int right = 0;
-                for (char c : str.toCharArray()) {
-                    if (c == '(') {
-                        left++;
-                    }
-                    if (c == ')') {
-                        right++;
-                    }
-                }
+            char[][] charsArray = charsList.toArray(new char[0][0]);
+            charsList.clear();
+            for (char[] chars : charsArray) {
+                char left = chars[0];
+                char right = chars[1];
                 if (left < n) {
-                    result.add(str + '(');
+                    if (i + 1 < 2 * n) {
+                        char[] newChars = Arrays.copyOf(chars, chars.length + 1);
+                        newChars[0] = (char) (left + 1);
+                        newChars[i + 2] = '(';
+                        charsList.add(newChars);
+                    } else {
+                        result.add(new String(chars).substring(2) + "(");
+                    }
                 }
                 if (left > right) {
-                    result.add(str + ')');
+                    if (i + 1 < 2 * n) {
+                        char[] newChars = Arrays.copyOf(chars, chars.length + 1);
+                        newChars[1] = (char) (right + 1);
+                        newChars[i + 2] = ')';
+                        charsList.add(newChars);
+                    } else {
+                        result.add(new String(chars).substring(2) + ")");
+                    }
                 }
             }
         }
